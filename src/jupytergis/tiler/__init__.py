540b1428b9c2e74701aeb1e2f9d1687a8b32c593
David Brochart <david.brochart@gmail.com>
2025-04-24 15:23:45 +0200

    First commit

---
+++ src/jupytergis/tiler/__init__.py
@@ -0,0 +1,9 @@
+from .gis_document import GISDocument as GISDocument
+from .server_extension import (
+    _jupyter_server_extension_paths,
+    _load_jupyter_server_extension,
+)
+
+
+__version__ = "0.1.0"
+load_jupyter_server_extension = _load_jupyter_server_extension
