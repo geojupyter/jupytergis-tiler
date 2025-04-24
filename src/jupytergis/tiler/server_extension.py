098c4654f6245ab1925ba40ae83fabfaadb2d6dc
David Brochart <david.brochart@gmail.com>
2025-04-24 16:22:46 +0200

    Fix

--- src/jupytergis/tiler/server_extension.py
+++ src/jupytergis/tiler/server_extension.py
@@ -9,7 +9,7 @@ def _load_jupyter_server_extension(serverapp):
 
     web_app = serverapp.web_app
     host_pattern = ".*$"
-    route_pattern = url_path_join(web_app.settings["base_url"], "/jupytergis/(.*)")
+    route_pattern = url_path_join(web_app.settings["base_url"], "/jupytergis_tiler/(.*)")
     web_app.add_handlers(host_pattern, [(route_pattern, JupyterGISHandler)])
 
 
@@ -18,4 +18,4 @@ def _jupyter_server_extension_paths():
     Returns a list of dictionaries with metadata describing
     where to find the `_load_jupyter_server_extension` function.
     """
-    return [{"module": "jupytergis_server"}]
+    return [{"module": "jupytergis.tiler"}]
